616f6065807d6764f769db17937e7d4e494dadbb
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-03 13:06:10 +0000

    refined; inner representation of data instances does not use hashes
    Fri Nov 15 13:33:45 CET 2013
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@12117 7cf035df-8472-40d0-8fbe-a733a24d2855

--- scripts/filter_feat.pl
+++ scripts/filter_feat.pl
@@ -29,12 +29,15 @@ if (keys %in_hash > 0) {
     print $_ while (<STDIN>);
 }
 
-while (<STDIN>) {
-    my ($feats, $class) = Treex::Tool::ML::TabSpace::Util::parse_line($_, {split_key_val => 1});
-    print "\n" if (!defined $feats);
+while ( my $instance = Treex::Tool::ML::TabSpace::Util::parse_singleline(*STDIN, {split_key_val => 1}) ) {
+    if (!@$instance) {
+        print "\n";
+        next;
+    }
+    my ($feats, $class) = @$instance;
 
     my @filt_feats = grep { $print xor $hash{$_->[0]} } @$feats;
     next if (!@filt_feats);
-    my $str = Treex::Tool::ML::TabSpace::Util::format_line(\@filt_feats, $class);
+    my $str = Treex::Tool::ML::TabSpace::Util::format_singleline(\@filt_feats, $class);
     print $str;
 }
--- scripts/vw_convert.pl
+++ scripts/vw_convert.pl
@@ -4,68 +4,97 @@ use warnings;
 use strict;
 
 use Getopt::Long;
+use Treex::Tool::ML::TabSpace::Util;
+use Treex::Tool::ML::VowpalWabbit::Util;
 
-my $test;
-my $multi;
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+
+#my $test;
+my $multi = 0;
 GetOptions(
-    "test" => \$test,
-    "multi|m=i" => \$multi
+#    "test" => \$test,
+    "multi|m" => \$multi
 );
 
-sub print_multiline {
-    my ($instances, $label_num, $is_test) = @_;
-    
-    foreach my $instance (@$instances) {
-        print "shared |s " . $instance->{feats} . "\n";
-        foreach my $label (1 .. $label_num) {
-            my $loss = $label eq $instance->{class} ? 0 : 1;
-            if ($is_test) {
-                print "$label " . $instance->{class} . "|t $label\n";
-            }
-            else {
-                print "$label:$loss |t $label\n";
-            }
-        }
-        print "\n";
-    }
+my ($parser, $formatter);
+if ($multi) {
+    $parser = \&Treex::Tool::ML::TabSpace::Util::parse_multiline;
+    $formatter = \&Treex::Tool::ML::VowpalWabbit::Util::format_multiline;
+}
+else {
+    $parser = \&Treex::Tool::ML::TabSpace::Util::parse_singleline;
+    $formatter = \&Treex::Tool::ML::VowpalWabbit::Util::format_singleline;
+}
+while ( my $instance = $parser->(*STDIN) ) {
+    print $formatter->(@$instance);
 }
 
-sub print_singleline {
-    my ($instances, $is_test) = @_;
-    
-    foreach my $instance (@$instances) {
-        my $comment = "";
-        if ($is_test) {
-            $comment = $instance->{class};
-        }
-        print $instance->{class} . " $comment| " . $instance->{feats} . "\n";
-    }
-}
-
-
-
-# first run over the data to collect all labels
-
-#my %all_cs_lemmas = ();
-my @instances = ();
-
-while (my $line = <STDIN>) {
-    chomp($line);
-    $line =~ s/:/__COL__/g;
-    $line =~ s/\|/__PIPE__/g;
-    my ($class, $feats) = split /\t/, $line;
-
-    push @instances, {class => $class, feats => $feats };
-
-    #$all_cs_lemmas{$cs_lemma}++;
-}
-
-#my @sorted_labels = sort {$a <=> $b} keys %all_cs_lemmas;
-
-# second run to convert the data into the VW format
-
-if (defined $multi) {
-    print_multiline(\@instances, $multi, $test);
-} else {
-    print_singleline(\@instances, $test);
-}
+#sub print_multiline {
+#    my ($instances, $label_num, $is_test) = @_;
+#    
+#    foreach my $instance (@$instances) {
+#        print "shared |s " . $instance->{feats} . "\n";
+#        foreach my $label (1 .. $label_num) {
+#            my $loss = $label eq $instance->{class} ? 0 : 1;
+#            if ($is_test) {
+#                print "$label " . $instance->{class} . "|t $label\n";
+#            }
+#            else {
+#                print "$label:$loss |t $label\n";
+#            }
+#        }
+#        print "\n";
+#    }
+#}
+#
+#sub print_singleline {
+#    my ($instances, $is_test) = @_;
+#    
+#    foreach my $instance (@$instances) {
+#        my $comment = "";
+#        if ($is_test) {
+#            $comment = $instance->{class};
+#        }
+#        print $instance->{class} . " $comment| " . $instance->{feats} . "\n";
+#    }
+#}
+#
+#
+#
+## first run over the data to collect all labels
+#
+##my %all_cs_lemmas = ();
+#my @instances = ();
+#
+#while (my $line = <STDIN>) {
+#    my ($feats, $label) = Treex::Tool::ML::TabSpace::Util::parse_line($line);
+#    
+#    # empty line
+#    if (!defined $feats) {
+#        print "\n";
+#    }
+#
+#
+#
+#
+#
+#    chomp($line);
+#    $line =~ s/:/__COL__/g;
+#    $line =~ s/\|/__PIPE__/g;
+#    my ($class, $feats) = split /\t/, $line;
+#
+#    push @instances, {class => $class, feats => $feats };
+#
+#    #$all_cs_lemmas{$cs_lemma}++;
+#}
+#
+##my @sorted_labels = sort {$a <=> $b} keys %all_cs_lemmas;
+#
+## second run to convert the data into the VW format
+#
+#if (defined $multi) {
+#    print_multiline(\@instances, $multi, $test);
+#} else {
+#    print_singleline(\@instances, $test);
+#}
