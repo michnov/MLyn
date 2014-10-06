0691a6516dc6d11e03c4e2a267b6a4971164a31c
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:21:04 +0000

    forgot to update file_stem generator here
    Mon Aug  4 19:01:42 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13512 7cf035df-8472-40d0-8fbe-a733a24d2855

--- scripts/data_split.sh
+++ scripts/data_split.sh
@@ -7,7 +7,7 @@ unlabeled_count=`ls $unlabeled_data | wc -l`
 if [ $unlabeled_count -eq 1 ]; then
     if [ ! -z $unlabeled_split_size ]; then
         $ML_FRAMEWORK_DIR/log.sh DEBUG "UNLABELED SPLIT SIZE = $unlabeled_split_size"
-        file_stem=`make -s -f $ML_FRAMEWORK_DIR/makefile.common file_stem FILE=$unlabeled_data`
+        file_stem=`$ML_FRAMEWORK_DIR/scripts/file_stem.pl "$unlabeled_data"`
         $ML_FRAMEWORK_DIR/log.sh DEBUG "FILE_STEM = $file_stem"
         zcat $unlabeled_data | $ML_FRAMEWORK_DIR/scripts/split_on_empty_line.pl $unlabeled_split_size $run_dir/data/$file_stem.part
         unlabeled_data=$run_dir/data/$file_stem.part*
