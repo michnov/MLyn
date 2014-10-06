2f7e1118c47a47bddb316573be0e67e04da900a2
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:20:14 +0000

    recursive run_experiment prepended with run.sh which print out the basic info
    Wed Jul 23 18:41:59 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13487 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source $ML_FRAMEWORK_DIR/params.sh
+prepare_run_dir
+config_file=${params[RUN_DIR]}/config
+save_params params $config_file
+
+stats_file=${params[RUN_DIR]}/stats
+
+echo -en "INFO:\t" >> $stats_file
+echo -en ${params[DATE]}"\t" >> $stats_file
+echo -e "`git rev-parse --abbrev-ref HEAD`:`git rev-parse HEAD | cut -c 1-10`" >> $stats_file
+
+$ML_FRAMEWORK_DIR/run_experiment.sh -f $config_file
+
+$ML_FRAMEWORK_DIR/log.sh INFO "Complete results stored in: $stats_file"
