8d4b738004228105a0194fad4280dcaad5518790
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:18:01 +0000

    debug print
    Mon Jun  2 22:22:37 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13423 7cf035df-8472-40d0-8fbe-a733a24d2855

--- scripts/filter_by_loss.pl
+++ scripts/filter_by_loss.pl
@@ -21,11 +21,19 @@ if (@ARGV < 1) {
 }
 my $loss_limit = $ARGV[0];
 
+my $all_count = 0;
+my $ok_count = 0;
+
 while ( my $instance = Treex::Tool::ML::TabSpace::Util::parse_multiline(*STDIN) ) {
     my ($feats, $losses) = @$instance;
     my $min_loss = min @$losses;
     #print STDERR "$min_loss\n";
     if ($min_loss < $loss_limit) {
         print Treex::Tool::ML::TabSpace::Util::format_multiline(@$instance);
+        $ok_count++;
+    }
+    $all_count++;
+    if ($all_count % 1000 == 0) {
+        print STDERR "FILTER_BY_LOSS: filtered $ok_count out of $all_count, so far\n";
     }
 }
