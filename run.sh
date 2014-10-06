a28f2424e686e573e21cd499616c1c5e2180be30
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:20:21 +0000

    bugfixin
    Wed Jul 23 20:14:07 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13491 7cf035df-8472-40d0-8fbe-a733a24d2855

--- run.sh
+++ run.sh
@@ -5,13 +5,17 @@ prepare_run_dir
 config_file=${params[RUN_DIR]}/config
 save_params params $config_file
 
+info_file=${params[RUN_DIR]}/info
 stats_file=${params[RUN_DIR]}/stats
 
-echo -en "INFO:\t" >> $stats_file
-echo -en ${params[DATE]}"\t" >> $stats_file
-echo -en "`git rev-parse --abbrev-ref HEAD`:`git rev-parse HEAD | cut -c 1-10`\t" >> $stats_file
-echo ${params[D]}
+echo -en "INFO:\t" >> $info_file
+echo -en ${params[DATE]}"\t" >> $info_file
+echo -en "`git rev-parse --abbrev-ref HEAD`:`git rev-parse HEAD | cut -c 1-10`\t" >> $info_file
+echo ${params[D]} >> $info_file
 
 $ML_FRAMEWORK_DIR/run_experiment.sh -f $config_file
+cat $stats_file >> $info_file
+cp $info_file $stats_file
+rm $info_file
 
 $ML_FRAMEWORK_DIR/log.sh INFO "Complete results stored in: $stats_file"
