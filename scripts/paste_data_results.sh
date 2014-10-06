5a081985f330ae855777256b3141dda7fde1fc39
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:20:35 +0000

    bugfix
    Thu Jul 31 15:54:35 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13498 7cf035df-8472-40d0-8fbe-a733a24d2855

--- scripts/paste_data_results.sh
+++ scripts/paste_data_results.sh
@@ -6,8 +6,8 @@ mkdir $tmp_dir
 gzipped_data=$1
 results=$2
 
-zcat $gzipped_data > $tmp_dir/data
 cat $results | cut -f1 -d' ' | perl -e '$ni = 1; while (<STDIN>) { chomp $_; if ($ni) { print "\n"; } if ($_ =~ /^$/) {$ni = 1;} else {$ni = 0;}; print "$_\n"; }' > $tmp_dir/results
-zcat $gzipped_data | paste $tmp_dir/results - | perl -ne 'chomp $_; my ($res, $x, @rest) = split /\t/, $_; if ($res =~ /^$/) {print join "\t", ($x, @rest); print "\n";} else { ($first, $second, @parts) = split /:/, $x; print join "\t", (join ":", $first.":".$res.$second, @parts), @rest; print "\n";}'
+zcat $gzipped_data | paste -d '|' $tmp_dir/results - > $tmp_dir/data
+cat $tmp_dir/data | perl -ne 'chomp $_; my ($new_value, $x, @rest) = split /\|/, $_; if ($new_value =~ /^$/) {print join "|", ($x, @rest); print "\n";} else { my ($label, $tag) = split / /, $x; my ($idx, $value) = split /:/, $label; print join "|", ($idx.":".$new_value." ".$tag, @rest); print "\n";}'
 
 rm -rf $tmp_dir
