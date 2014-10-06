d18728da3795e485a92eeb3ae7da8824cc7175b5
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:21:41 +0000

    train_test can evaluate multipple test sets at once
    Sat Aug  9 21:44:58 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13531 7cf035df-8472-40d0-8fbe-a733a24d2855

--- train_test.sh
+++ train_test.sh
@@ -13,16 +13,30 @@
 
 function train_test() {
     run_dir=${params[RUN_DIR]}
-    train_data=${params[TRAIN_DATA]}
-    test_data=${params[TEST_DATA]}
 
-    make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval eval CONFIG_FILE=$config_file TRAIN_DATA=$train_data TEST_DATA=$train_data > $run_dir/stats.numbers
-    make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval eval CONFIG_FILE=$config_file TRAIN_DATA=$train_data TEST_DATA=$test_data >> $run_dir/stats.numbers
+    train_data=${params[TRAIN_DATA]}
     
+    test_data_names="${params[TEST_DATA_LIST]}"
+    if [ -z "$test_data_names" ]; then
+        test_data_names="TRAIN_DATA TEST_DATA"
+    fi
+   
+    # make the files empty if existing
+    cat /dev/null > $run_dir/stats.numbers
+    cat /dev/null > $run_dir/stats.header
+    
+    for data_name in $test_data_names; do
+        data_path=${params[$data_name]}
+        if [ -z "$data_path" ]; then
+            $ML_FRAMEWORK_DIR/log.sh ERROR "Dataset $data_name is not defined."
+            exit
+        fi
+        make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval eval CONFIG_FILE=$config_file TRAIN_DATA=$train_data TEST_DATA=$data_path >> $run_dir/stats.numbers
+        print_ranking_header $data_name >> $run_dir/stats.header
+    done
+
     # printing the results
     echo -e "ML_METHOD:\t" ${params[ML_METHOD]} ${params[ML_PARAMS]} > $run_dir/stats
-    print_ranking_header "TRAIN" > $run_dir/stats.header
-    print_ranking_header "TEST" >> $run_dir/stats.header
     paste $run_dir/stats.header $run_dir/stats.numbers >> $run_dir/stats
     rm $run_dir/stats.header $run_dir/stats.numbers
 }
