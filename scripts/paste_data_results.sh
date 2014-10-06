3efdef7a1078bf866556eb0e83f3dbe59e104712
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:18:35 +0000

    fix
    Wed Jun 25 19:21:05 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13439 7cf035df-8472-40d0-8fbe-a733a24d2855

--- scripts/paste_data_results.sh
+++ scripts/paste_data_results.sh
@@ -8,6 +8,6 @@ results=$2
 
 zcat $gzipped_data > $tmp_dir/data
 cat $results | cut -f1 -d' ' | perl -e '$ni = 1; while (<STDIN>) { chomp $_; if ($ni) { print "\n"; } if ($_ =~ /^$/) {$ni = 1;} else {$ni = 0;}; print "$_\n"; }' > $tmp_dir/results
-zcat $gzipped_data | paste $tmp_dir/results - | perl -ne 'chomp $_; my ($res, $x) = split /\t/, $_; if ($res =~ /^$/) {print "$x\n";} else { ($first, $second, @parts) = split /:/, $x; print join ":", ($first.":".$res.$second, @parts); print "\n";}'
+zcat $gzipped_data | paste $tmp_dir/results - | perl -ne 'chomp $_; my ($res, $x, @rest) = split /\t/, $_; if ($res =~ /^$/) {print join "\t", ($x, @rest); print "\n";} else { ($first, $second, @parts) = split /:/, $x; print join "\t", (join ":", $first.":".$res.$second, @parts), @rest; print "\n";}'
 
 rm -rf $tmp_dir
