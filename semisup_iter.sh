df3fed45905cd547ffb120ce33f0d367b83c5116
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:20:42 +0000

    done* files more specified - to distinguish between en and cs runs
    Fri Aug  1 11:37:49 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13502 7cf035df-8472-40d0-8fbe-a733a24d2855

--- semisup_iter.sh
+++ semisup_iter.sh
@@ -14,21 +14,25 @@ make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval eval CONFIG_FILE=$config_f
     
 max_loss=${params[MAX_LOSS]}
 
+all_base=`basename "${params[UNLABELED_DATA]}"`
+#$ML_FRAMEWORK_DIR/log.sh DEBUG "BASENAME: $all_base"
+
 file_i=1
 for file_part in ${params[UNLABELED_DATA]}; do
-    file_i_str=`printf "%03d" $file_i`
+    #file_i_str=`printf "%03d" $file_i`
+    base=`basename $file_part`
     
     result_file=`make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval result_path CONFIG_FILE=$config_file RUN_DIR=$run_dir TEST_DATA=$file_part`
     
     mkdir -p $run_dir/data
-    sys_labeled_data=$run_dir/data/`basename $file_part`
+    sys_labeled_data=$run_dir/data/$base
     
     run_in_parallel \
         "make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval test CONFIG_FILE=$config_file RUN_DIR=$run_dir TEST_DATA=$file_part; \
             $ML_FRAMEWORK_DIR/log.sh INFO \"Adding system labels to the unlabeled data, if the minimum loss is <= $max_loss: $file_part + $result_file => $sys_labeled_data\"; \
             $ML_FRAMEWORK_DIR/scripts/paste_data_results.sh $file_part $result_file | $ML_FRAMEWORK_DIR/scripts/filter_by_loss.pl $max_loss | $ML_FRAMEWORK_DIR/scripts/discretize_losses.pl | gzip -c > $sys_labeled_data; \
-            touch $run_dir/done.$file_i_str" \
-        "unlabeled.part.$file_i_str" -50 $run_dir/log 0
+            touch $run_dir/done.$base" \
+        "unlabeled.$base" -50 $run_dir/log 0
 
     ((file_i++))
 done
@@ -36,7 +40,7 @@ done
 # wait until all experiments are acomplished
 $ML_FRAMEWORK_DIR/log.sh INFO "Waiting for all the experiments to be completed..."
 unlabeled_count=`ls ${params[UNLABELED_DATA]} | wc -l`
-while [ `ls $run_dir/done.* 2> /dev/null | wc -l` -lt $unlabeled_count ]; do
-    $ML_FRAMEWORK_DIR/log.sh DEBUG `ls $run_dir/done.* 2> /dev/null | wc -l` $unlabeled_count
+while [ `ls $run_dir/done.$all_base 2> /dev/null | wc -l` -lt $unlabeled_count ]; do
+    $ML_FRAMEWORK_DIR/log.sh DEBUG `ls $run_dir/done.$all_base 2> /dev/null | wc -l` $unlabeled_count
     sleep 10
 done
