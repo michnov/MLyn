55909ba53a0405ada7cbe865c8730318f606062b
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:17:32 +0000

    params manipulation in a separate script
    Wed May 28 22:17:06 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13409 7cf035df-8472-40d0-8fbe-a733a24d2855

--- common.sh
+++ common.sh
@@ -1,49 +1,5 @@
 #!/bin/bash
 
-# loading parameters into a global hash
-# parameters can be specified both in a file: argument -f <file>
-# or directly as a commmand arg
-# the format of parameters is: name=value
-# INPUT:
-#   1) the name of a global hash
-#   2) all arguments passed in a command
-function load_params()
-{
-    config_var=$1
-    shift
-
-    local OPTIND
-    while getopts ":f:" opt; do
-        case $opt in
-            f) config_file=$OPTARG
-                ;;
-            \?) echo "Invalid option: -$OPTARG" >&2
-                ;;
-        esac
-    done
-
-    sed_cmd='s/^\([^=]*\)=\(.*\)$/'$config_var'[\1]="\2";/'
-    config_script=`cat $config_file | grep -v "^#" | sed $sed_cmd`
-    shift $(($OPTIND - 1))
-    
-    config_script+=`echo "$@" | sed 's/ /\n/g' | sed $sed_cmd`
-    eval $config_script
-}
-
-function save_params()
-{
-    config_var=$1
-    output_file=$2
-    if [ -f $output_file ]; then
-        rm $output_file;
-    fi
-    all_keys=`eval 'echo ${!'$config_var'[@]}'`
-    for key in $all_keys; do
-        value=`eval 'echo ${'$config_var'['$key']}'`
-        echo $key=$value >> $output_file
-    done
-}
-
 function run_in_parallel()
 {
     cmd=$1
@@ -59,10 +15,3 @@ function run_in_parallel()
         eval $cmd
     fi
 }
-
-#declare -A params
-#load_params params "$@"
-#echo "POzdrav: ${params[file]}"
-#echo "Blbost: ${params[skuska]}"
-#echo "ROBO: ${params[ROBO]}"
-#echo "FUCK: ${params[FUCK]}"
---
+++ params.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# loading parameters into a global hash
+# parameters can be specified both in a file: argument -f <file>
+# or directly as a commmand arg
+# the format of parameters is: name=value
+# INPUT:
+#   1) the name of a global hash
+#   2) all arguments passed in a command
+function load_params()
+{
+    config_var=$1
+    shift
+
+    local OPTIND
+    while getopts ":f:" opt; do
+        case $opt in
+            f) config_file=$OPTARG
+                ;;
+            \?) echo "Invalid option: -$OPTARG" >&2
+                ;;
+        esac
+    done
+
+    sed_cmd='s/^\([^=]*\)=\(.*\)$/'$config_var'[\1]="\2";/'
+    config_script=`cat $config_file | grep -v "^#" | sed $sed_cmd`
+    shift $(($OPTIND - 1))
+    
+    config_script+=`echo "$@" | sed 's/ /\n/g' | sed $sed_cmd`
+    eval $config_script
+}
+
+function save_params()
+{
+    config_var=$1
+    output_file=$2
+    if [ -f $output_file ]; then
+        rm $output_file;
+    fi
+    all_keys=`eval 'echo ${!'$config_var'[@]}'`
+    for key in $all_keys; do
+        value=`eval 'echo ${'$config_var'['$key']}'`
+        echo $key=$value >> $output_file
+    done
+}
+
+#declare -A params
+#load_params params "$@"
+#echo "POzdrav: ${params[file]}"
+#echo "Blbost: ${params[skuska]}"
+#echo "ROBO: ${params[ROBO]}"
+#echo "FUCK: ${params[FUCK]}"
