d42b037fcaa36ad0a2ff265ae52bf6c7f14aaaee
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:18:27 +0000

    train test with a preprocessing separated
    Mon Jun 23 18:55:17 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13435 7cf035df-8472-40d0-8fbe-a733a24d2855

--- train_test.sh
+++ train_test.sh
@@ -12,5 +12,12 @@ config_file=$run_dir/config
 #log_dir=$run_dir/log
 #mkdir -p $log_dir
 
-make -s -f makefile.train_test_eval eval CONFIG_FILE=$config_file TEST_DATA=${params[TRAIN_DATA]} > >(tee -a $run_dir/stats)
-make -s -f makefile.train_test_eval eval CONFIG_FILE=$config_file TEST_DATA=${params[TEST_DATA]} > >(tee -a $run_dir/stats)
+make -s -f makefile.preprocess preprocess CONFIG_FILE=$config_file DATA=${params[TRAIN_DATA]}
+train_ready=`make -s -f makefile.preprocess data_ready_path CONFIG_FILE=$config_file DATA=${params[TRAIN_DATA]}`
+echo $train_ready
+make -s -f makefile.train_test_eval eval CONFIG_FILE=$config_file TRAIN_DATA=$train_ready TEST_DATA=$train_ready > >(tee -a $run_dir/stats)
+
+make -s -f makefile.preprocess preprocess CONFIG_FILE=$config_file DATA=${params[TEST_DATA]}
+test_ready=`make -s -f makefile.preprocess data_ready_path CONFIG_FILE=$config_file DATA=${params[TEST_DATA]}`
+echo $test_ready
+make -s -f makefile.train_test_eval eval CONFIG_FILE=$config_file TRAIN_DATA=$train_ready TEST_DATA=$test_ready > >(tee -a $run_dir/stats)
