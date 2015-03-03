0ce5f4df29cfa27aa19b73f37878b0e6c9e14a7d
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-03-03 15:26:28 +0000

    adjusted to the results format in a new vwersion of VW
    Sun Feb  1 23:59:03 CET 2015
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@14597 7cf035df-8472-40d0-8fbe-a733a24d2855

--- scripts/results_to_triples.pl
+++ scripts/results_to_triples.pl
@@ -10,31 +10,39 @@ GetOptions(
     "ranking" => \$ranking,
 );
 
+my %pred_costs = ();
+my @true_idx = ();
+my $self_idx;
 while (my $line = <STDIN>) {
     chomp $line;
-    next if ($line =~ /^\s*$/);
-    my ($pred_str, $true_str) = split / /, $line;
-    
     if ($ranking) {
-        my $pred_idx = int($pred_str);
-        next if ($pred_idx == 0);
-
-        my ($true_idx_str, $self_idx) = split /-/, $true_str;
-        my @true_idx = split /,/, $true_idx_str;
-
-        my $true_count = scalar @true_idx;
-        my $pred_count = 1;
-        my $both_count = (any {$_ == $pred_idx} @true_idx) ? 1 : 0;
-        if (defined $self_idx) {
-            $true_count = 0 if (any {$_ == $self_idx} @true_idx);
-            $pred_count = 0 if ($pred_idx == $self_idx);
-            $both_count = 0 if (!$true_count && !$pred_count); 
+        if ($line =~ /^\s*$/) {
+            my ($pred_idx, @other_idx) = sort {$pred_costs{$a} <=> $pred_costs{$b}} keys %pred_costs;
+            my $true_count = scalar @true_idx;
+            my $pred_count = 1;
+            my $both_count = (any {$_ == $pred_idx} @true_idx) ? 1 : 0;
+            if (defined $self_idx) {
+                $true_count = 0 if (any {$_ == $self_idx} @true_idx);
+                $pred_count = 0 if ($pred_idx == $self_idx);
+                $both_count = 0 if (!$true_count && !$pred_count); 
+            }
+            print join " ", ($true_count, $pred_count, $both_count);
+            print "\n";
+            %pred_costs = (); @true_idx = (); $self_idx = undef;
         }
-
-        print join " ", ($true_count, $pred_count, $both_count);
+        else {
+            my ($pred_str, $true_str) = split / /, $line;
+            my ($pred_idx, $pred_cost) = split /:/, $pred_str;
+            $pred_costs{$pred_idx} = $pred_cost;
+            if (!@true_idx) {
+                (my $true_idx_str, $self_idx) = split /-/, $true_str;
+                @true_idx = split /,/, $true_idx_str;
+            }
+        }
     }
-    else {
-        print join " ", (1, 1, $pred_str == $true_str ? 1 : 0);
-    }
-    print "\n";
+    # TODO: non-ranking should be adjusted to the new version of VW
+    #else {
+    #    print join " ", (1, 1, $pred_str == $true_str ? 1 : 0);
+    #    print "\n";
+    #}
 }
