6b4597134026c894957bce1071a725b1e17d8c35
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-04-30 09:08:35 +0000

    co-training-ali adjusted to pooling and not using initial models
    Tue Mar 31 01:23:12 CEST 2015
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@14899 7cf035df-8472-40d0-8fbe-a733a24d2855

--- co_training_ali.sh
+++ co_training_ali.sh
@@ -18,12 +18,23 @@ function co_training_ali() {
     $ML_FRAMEWORK_DIR/log.sh DEBUG "L1_UNLAB_DATA: $l1_unlabeled_data"
     $ML_FRAMEWORK_DIR/log.sh DEBUG "L2_UNLAB_DATA: $l2_unlabeled_data"
     
+    l1_unlabeled_base=`basename "$l1_unlabeled_data"`
+    l2_unlabeled_base=`basename "$l2_unlabeled_data"`
+    
+    # count the number of instances (so far only for the ranking-style data)
+    $ML_FRAMEWORK_DIR/log.sh INFO "Counting the number of instances in L1_UNLAB_DATA. This might take a few minutes..."
+    for file in $l1_unlabeled_data; do
+        #$ML_FRAMEWORK_DIR/log.sh DEBUG "Storing $file into: $run_dir/data/instances_per_part.l1.$l1_unlabeled_base"
+        zcat $file | $ML_FRAMEWORK_DIR/scripts/count_ranking_instances.pl >> $run_dir/data/instances_per_part.l1.$l1_unlabeled_base
+    done
+    $ML_FRAMEWORK_DIR/log.sh INFO "Counting the number of instances in L2_UNLAB_DATA. This might take a few minutes..."
+    for file in $l2_unlabeled_data; do
+        #$ML_FRAMEWORK_DIR/log.sh DEBUG "Storing into: $run_dir/data/instances_per_part.l2.$l2_unlabeled_base"
+        zcat $file | $ML_FRAMEWORK_DIR/scripts/count_ranking_instances.pl >> $run_dir/data/instances_per_part.l2.$l2_unlabeled_base
+    done
+    
     ######################## Self-training iterations ##########################
     
-    # different settings of ML_PARAMS for the 0th and next iterations
-    if [ -z "${params[ML_PARAMS_FOR_UNLABELED]}" ]; then
-        params[ML_PARAMS_FOR_UNLABELED]=${params[ML_PARAMS]}
-    fi
     ml_params=${params[ML_PARAMS]}
 
     # iterations
@@ -42,6 +53,8 @@ function co_training_ali() {
                 TEST_DATA=${params[L1_TEST_DATA]} \
                 UNLABELED_DATA=$l1_unlabeled_data \
                 ML_PARAMS=\"$ml_params\" \
+                ITER=$i \
+                UNLABELED_PART_SIZES=$run_dir/data/instances_per_part.l1.$l1_unlabeled_base \
                 INITIAL_MODEL=$l1_init_model \
                 RUN_DIR=$l1_iter_run_dir;
                 touch $run_dir/iter_$iter/done.semisup_iter.l1" \
@@ -53,24 +66,23 @@ function co_training_ali() {
                 TEST_DATA=${params[L2_TEST_DATA]} \
                 UNLABELED_DATA=$l2_unlabeled_data \
                 ML_PARAMS=\"$ml_params\" \
+                ITER=$i \
+                UNLABELED_PART_SIZES=$run_dir/data/instances_per_part.l2.$l2_unlabeled_base \
                 INITIAL_MODEL=$l2_init_model \
                 RUN_DIR=$l2_iter_run_dir;
                 touch $run_dir/iter_$iter/done.semisup_iter.l2" \
             "semisup_iter.l2.$iter" -30 $run_dir/log 0
         wait_for_jobs "$run_dir/iter_$iter/done.semisup_iter.*" 2
 
-        l1_base=`basename "$l1_unlabeled_data"`
-        l2_base=`basename "$l2_unlabeled_data"`
-
         # transfer the labeling via alignment
-        l1_labeled_data=$l1_iter_run_dir/data/$l1_base
-        l2_labeled_data=$l2_iter_run_dir/data/$l2_base
+        l1_labeled_data=$l1_iter_run_dir/data/$l1_unlabeled_base
+        l2_labeled_data=$l2_iter_run_dir/data/$l2_unlabeled_base
         transfer_labels_via_align "$l1_unlabeled_data" "$l2_labeled_data" $l1_iter_run_dir
         transfer_labels_via_align "$l2_unlabeled_data" "$l1_labeled_data" $l2_iter_run_dir
         wait_for_jobs "$l1_iter_run_dir/done.aligned.*" `ls $l1_unlabeled_data | wc -l`
         wait_for_jobs "$l2_iter_run_dir/done.aligned.*" `ls $l2_unlabeled_data | wc -l`
-        l1_align_labeled_data=$l1_iter_run_dir/data/aligned.$l1_base
-        l2_align_labeled_data=$l2_iter_run_dir/data/aligned.$l2_base
+        l1_align_labeled_data=$l1_iter_run_dir/data/aligned.$l1_unlabeled_base
+        l2_align_labeled_data=$l2_iter_run_dir/data/aligned.$l2_unlabeled_base
 
         # merging stats
         echo $iter > $run_dir/iter_$iter/stats
@@ -78,28 +90,38 @@ function co_training_ali() {
         cat $run_dir/iter_$iter/l2/stats >> $run_dir/iter_$iter/stats
 
         # set params for the next iteration
-        if [ $split -eq 0 ]; then
-            l1_train_data="$l1_labeled_data $l1_align_labeled_data"
-            l2_train_data="$l2_labeled_data $l2_align_labeled_data"
-            $ML_FRAMEWORK_DIR/log.sh INFO "Not split; using both the labeled and align-transferred data as a training set for the next iteration."
-        else
-            l1_train_data=$l1_align_labeled_data
-            l2_train_data=$l2_align_labeled_data
+        if [ -n "$split" -a "$split" -ne 0 ]; then
+            l1_system_labeled_data=$l1_align_labeled_data
+            l2_system_labeled_data=$l2_align_labeled_data
             $ML_FRAMEWORK_DIR/log.sh INFO "Split; using only the align-transferred data as a training set for the next iteration."
+        else
+            l1_system_labeled_data="$l1_labeled_data $l1_align_labeled_data"
+            l2_system_labeled_data="$l2_labeled_data $l2_align_labeled_data"
+            $ML_FRAMEWORK_DIR/log.sh INFO "Not split; using both the labeled and align-transferred data as a training set for the next iteration."
         fi
+        
+        if [ -n "$delible" -a "$delible" -ne 0 ]; then
+            l1_all_system_labeled_data=$l1_system_labeled_data
+            l2_all_system_labeled_data=$l2_system_labeled_data
+            $ML_FRAMEWORK_DIR/log.sh INFO "Delible; using gold-labeled training data and $l1_all_system_labeled_data (or $l2_all_system_labeled_data) as a training data for the next iteration."
+        else
+            if [ -n $prev_iter ]; then
+                l1_prev_iter_all_system_labeled_data=$run_dir/iter_$prev_iter/l1/data/all.system_labeled.table
+                l2_prev_iter_all_system_labeled_data=$run_dir/iter_$prev_iter/l2/data/all.system_labeled.table
+            fi
+            l1_all_system_labeled_data=$l1_iter_run_dir/data/all.system_labeled.table 
+            l2_all_system_labeled_data=$l2_iter_run_dir/data/all.system_labeled.table 
+            zcat $l1_prev_iter_all_system_labeled_data $l1_system_labeled_data | gzip -c > $l1_all_system_labeled_data
+            zcat $l2_prev_iter_all_system_labeled_data $l2_system_labeled_data | gzip -c > $l2_all_system_labeled_data
+            $ML_FRAMEWORK_DIR/log.sh INFO "Not delible; using gold-labeled training data and cumulated data in $l1_all_system_labeled_data (or $l2_all_system_labeled_data) as a training data for the next iteration."
+        fi
+        
+        l1_train_data="${params[L1_TRAIN_DATA]} $l1_all_system_labeled_data"
+        l2_train_data="${params[L2_TRAIN_DATA]} $l2_all_system_labeled_data"
         $ML_FRAMEWORK_DIR/log.sh DEBUG "L1 train data: $l1_train_data"
         $ML_FRAMEWORK_DIR/log.sh DEBUG "L2 train data: $l2_train_data"
-
-        if [ $delible -eq 0 ]; then
-            l1_init_model=`make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval model_path CONFIG_FILE=$config_file RUN_DIR=$run_dir/iter_$iter/l1 TRAIN_DATA=$l1_train_data`
-            l2_init_model=`make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval model_path CONFIG_FILE=$config_file RUN_DIR=$run_dir/iter_$iter/l2 TRAIN_DATA=$l2_train_data`
-            $ML_FRAMEWORK_DIR/log.sh INFO "Not delible; using cumulated models $l1_init_model and $l2_init_model as initial models for the next iteration."
-        else
-            l1_init_model=`make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval model_path CONFIG_FILE=$config_file RUN_DIR=$run_dir/iter_000/l1 TRAIN_DATA=${params[L1_TRAIN_DATA]}`
-            l2_init_model=`make -s -f $ML_FRAMEWORK_DIR/makefile.train_test_eval model_path CONFIG_FILE=$config_file RUN_DIR=$run_dir/iter_000/l2 TRAIN_DATA=${params[L2_TRAIN_DATA]}`
-            $ML_FRAMEWORK_DIR/log.sh INFO "Delible; using gold-labeled models $l1_init_model and $l2_init_model as initial models for the next iteration."
-        fi
-        ml_params=${params[ML_PARAMS_FOR_UNLABELED]}
+        
+        prev_iter=$iter
     done
 
     # the final iteration - just training and testing
