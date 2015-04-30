a5c17c0e64c09050bcec2e580cf4857ef71f2080
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-04-30 09:08:51 +0000

    vw data reader for python
    Fri Apr 10 11:42:32 CEST 2015
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@14907 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/vw_data.py
@@ -0,0 +1,52 @@
+import re
+
+class VowpalWabbitData:
+
+    ranking = True
+
+    def __init__(self, ranking=False):
+        self.ranking = ranking
+
+    def read(self, input_file):
+        X = []
+        Y = []
+        losses = []
+        for line in input_file:
+            line.rstrip("\n")
+            
+            # empty line indicates a new instance if ranking=1
+            if re.search("^\s+$", line):
+                if self.ranking:
+                    Y += [ x <= min(losses) for x in losses ]
+                    losses = []
+                continue
+                    
+            parts = line.split("|")
+            
+            # process the label part
+            label_part = parts.pop(0)
+            (label_loss, tag) = label_part.split()
+            (label, loss) = label_loss.split(":")
+            if self.ranking:
+                losses.append(loss)
+            else:
+                Y.append(label)
+
+            # process namespaces with features
+            all_feat_hash = {}
+            for ns_feats in parts:
+                feats = ns_feats.split()
+                
+                # identify the namespace
+                ns = ''
+                if not ns_feats.startswith(' '):
+                    ns = feats.pop(0)
+
+                # extract a feat hash
+                feats_name_valued = [ i+'=1' if not re.search("=", i) else i for i in feats ]
+                feat_hash = { (ns+"^"+k):v for (k,v) in (tuple(s.split("=")) for s in feats_name_valued) }
+                all_feat_hash.update(feat_hash)
+
+            X.append(all_feat_hash)
+        
+        return (X, Y)
