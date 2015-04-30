34763bd9afba307d104022841cd010cf510fb265
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-04-30 09:08:55 +0000

    vw reader included in sklearn.trainer
    Sat Apr 11 23:00:12 CEST 2015
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@14909 7cf035df-8472-40d0-8fbe-a733a24d2855

--- scripts/sklearn.train.py
+++ scripts/sklearn.train.py
@@ -1,31 +1,47 @@
 #!/usr/bin/env python
 
-import os, sys
+import os, sys, getopt
 import HTMLParser
 
 lib_path = os.path.abspath('lib')
 sys.path.append(lib_path)
 
 import model
+from vw_data import VowpalWabbitData
 
+def usage(app_name):
+    print >> sys.stderr, "Usage: " + app_name + " [--ranking] <baseline|svm|knn|naive_bayes|log_regression> <ml_params> <model_path>"
+
+# command line args parsing
 name = sys.argv.pop(0)
-if len(sys.argv) < 3:
-    print >> sys.stderr, "Usage: " + name + " <baseline|svm|knn|naive_bayes|log_regression> <ml_params> <model_path>"
+try:
+    optlist, args = getopt.getopt(sys.argv, '', ['ranking'])
+except getopt.GetoptError as err:
+    print str(err) # will print something like "option -a not recognized"
+    usage(name)
+    sys.exit(2)
+ranking = False
+for o,a in optlist:
+    if o == '--ranking':
+        ranking = True
+    else:
+        assert False, "unhandled option"
+if len(args) < 3:
+    usage(name)
+    sys.exit(2)
+ml_type = args[0]
+ml_param_str = args[1]
+model_path = args[2]
 
-h = HTMLParser.HTMLParser()
-params = h.unescape(sys.argv[1])
-
-model = model.Model(sys.argv[0],params)
-
+print >> sys.stderr, "Reading the data..."
 X = []
 Y = []
+in_data = VowpalWabbitData(ranking=ranking)
+(X, Y) = in_data.read(sys.stdin)
 
-for line in sys.stdin:
-    (y, x_str) = line.rstrip("\n").split("\t")
-    x_list = x_str.split(" ")
-    x_hash = { k:v for (k,v) in (tuple(s.split("=")) for s in x_list) }
-    Y.append(y)
-    X.append(x_hash)
-
+print >> sys.stderr, "Building the model..."
+h = HTMLParser.HTMLParser()
+params = h.unescape(ml_param_str)
+model = model.Model(ml_type,params)
 model.fit(X,Y)
-model.save(sys.argv[2])
+model.save(model_path)
