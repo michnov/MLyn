39b8d8fae7ebeaf33cfa622c5e5d0c562f94b285
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-04-30 09:08:45 +0000

    universal transformemr from 'shared' to 'non-shared' format
    Tue Apr  7 22:45:24 CEST 2015
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@14904 7cf035df-8472-40d0-8fbe-a733a24d2855

--- scripts/shared_to_nonshared.pl
+++ scripts/shared_to_nonshared.pl
@@ -3,23 +3,60 @@
 use warnings;
 use strict;
 
-while (my $line = <>) {
+sub parse_instance {
+    my ($line) = @_;
+    
     chomp $line;
-    my ($shared_first, @shared_rest) = split /\t/, $line;
-    my @shared_feats;
-    if ($shared_first =~ /^shared/) {
-        (my $shared_label, @shared_feats) = split /\|/, $shared_first;
+    my ($first, @rest) = split /\t/, $line;
+
+    my %ns_to_feats = ();
+    my ($label, @ns_feats) = split /\|/, $first;
+    foreach my $ns_feat (@ns_feats) {
+        my ($ns, @feats) = split / +/, $ns_feat;
+        my $old_feats = $ns_to_feats{$ns} // [];
+        push @$old_feats, @feats;
+        $ns_to_feats{$ns} = $old_feats;
+    }
+
+    return ($label, \%ns_to_feats, \@rest);
+}
+
+sub print_instance {
+    my ($label, $ns_to_feats, $rest) = @_;
+
+    my $ns_str = join " ", map {"|".$_." ".(join " ", @{$ns_to_feats->{$_}})} keys %$ns_to_feats;
+    print join "\t", ($label.$ns_str, @$rest);
+    print "\n";
+}
+
+sub merge_ns {
+    my (@nss) = @_;
+
+    my $merged_ns = {};
+    foreach my $ns (@nss) {
+        foreach my $ns_key (keys %$ns) {
+            my $old_feats = $merged_ns->{$ns_key} // [];
+            push @$old_feats, @{$ns->{$ns_key}};
+            $merged_ns->{$ns_key} = $old_feats;
+        }
+    }
+    return $merged_ns;
+}
+
+while (my $line = <>) {
+    
+    my ($shared_label, $shared_ns_to_feats, $shared_rest) = parse_instance($line);
+    if ($shared_label !~ /^shared/) {
+        next;
     }
 
     $line = <>;
-    chomp $line;
     while (defined $line && $line !~ /^\s*$/) {
-        my ($vw_example, @rest) = split /\t/, $line;
-        my $instance = join " |", ($vw_example, @shared_feats);
-        $instance = join "\t", ($instance, @shared_rest);
-        print $instance."\n";
+        my ($label, $ns_to_feats, $rest) = parse_instance($line);
+        my $merged_ns = merge_ns($ns_to_feats, $shared_ns_to_feats);
+        print_instance($label, $merged_ns, $rest);
+        
         $line = <>;
-        chomp $line;
     }
 
     print "\n";
